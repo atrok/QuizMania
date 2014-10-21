d23229b6d13a94fa992887ab1cc8d6a8ac59e7f7
atrok <andrey.trok@gmail.com>
2014-10-20 18:27:19 -0700

    added SuperCSV for testing

--- QuizMania/src/main/java/com/quizmania/repository/Game.java
+++ QuizMania/src/main/java/com/quizmania/repository/Game.java
@@ -15,44 +15,44 @@ public class Game {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 	
-	private Map<Long, String> var1;
-	private Map<Long, String>  var2;
-	private Map<Long, String>  var3;
-	private Map<Long, String>  var4;
+	private Map<String, String> var1;
+	private Map<String, String>  var2;
+	private Map<String, String>  var3;
+	private Map<String, String>  var4;
 	private long  answer;
 	private String description;
 	
 	public Game(){}
 
-	public Map<Long, String> getVar1() {
+	public Map<String, String> getVar1() {
 		return var1;
 	}
 
-	public void setVar1(Map<Long, String> var1) {
+	public void setVar1(Map<String, String> var1) {
 		this.var1 = var1;
 	}
 
-	public Map<Long, String> getVar2() {
+	public Map<String, String> getVar2() {
 		return var2;
 	}
 
-	public void setVar2(Map<Long, String> var2) {
+	public void setVar2(Map<String, String> var2) {
 		this.var2 = var2;
 	}
 
-	public Map<Long, String> getVar3() {
+	public Map<String, String> getVar3() {
 		return var3;
 	}
 
-	public void setVar3(Map<Long, String> var3) {
+	public void setVar3(Map<String, String> var3) {
 		this.var3 = var3;
 	}
 
-	public Map<Long, String> getVar4() {
+	public Map<String, String> getVar4() {
 		return var4;
 	}
 
-	public void setVar4(Map<Long, String> var4) {
+	public void setVar4(Map<String, String> var4) {
 		this.var4 = var4;
 	}
 
--- QuizMania/src/test/java/com/quizmania/server/TestGameSvcApi.java
+++ QuizMania/src/test/java/com/quizmania/server/TestGameSvcApi.java
@@ -4,61 +4,159 @@ import static org.junit.Assert.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.UUID;
 
+import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.supercsv.cellprocessor.Optional;
+import org.supercsv.cellprocessor.ParseBool;
+import org.supercsv.cellprocessor.ParseDate;
+import org.supercsv.cellprocessor.ParseInt;
+import org.supercsv.cellprocessor.constraint.LMinMax;
+import org.supercsv.cellprocessor.constraint.NotNull;
+import org.supercsv.cellprocessor.constraint.StrRegEx;
+import org.supercsv.cellprocessor.constraint.UniqueHashCode;
+import org.supercsv.cellprocessor.ift.CellProcessor;
+import org.supercsv.io.CsvMapReader;
+import org.supercsv.io.ICsvMapReader;
+import org.supercsv.prefs.CsvPreference;
 
 import com.quizmania.repository.Game;
 
 public class TestGameSvcApi {
 
-	private final String QUIZ_FILE="quizes.csv";
-	
-	List<Game> quiz;
+	private final static String QUIZ_FILE = "quizes.csv";
+
+	List<Game> quiz = null;
+	Set<String> SetOfUUID = null;
+
 	@Test
 	public void test() {
 
 		GameController gamecontroller = new GameController();
-		Game g=new Game();
+		Game g = new Game();
 
 		boolean ok = gamecontroller.addGameRecord(g);
-		
-		
+
 		// Test the servlet directly, without going through the network.
-		
+
 		assertTrue(ok);
-		
+
 		List<Game> games = gamecontroller.getListofGames();
 		assertTrue(games.contains(g));
 	}
-	
-	private Game createGame(Game g){
 
-	       // -define .csv file in app
- //       String fileNameDefined = QUIZ_FILE;
-        // -File class needed to turn stringName to actual file
-        File file = new File(QUIZ_FILE);
+	@BeforeClass
+	public static void setUpClass() throws Exception {
 
-        try{
-            // -read from filePooped with Scanner class
-            Scanner inputStream = new Scanner(file);
-            // hashNext() loops line-by-line
-            while(inputStream.hasNext()){
-                //read single line, put in string
-                String data = inputStream.next();
-                g.
-                System.out.println(data + "***");
-
-            }
-            // after loop, close scanner
-            inputStream.close();
-
-
-        }catch (FileNotFoundException e){
-
-            e.printStackTrace();
-        }
+		// -define .csv file in app
+		// String fileNameDefined = QUIZ_FILE;
+		// -File class needed to turn stringName to actual file
+		readWithCsvMapReader();
 	}
 
+	private Game createGame(){
+		   	Game g= new Game();
+	        File file = new File(QUIZ_FILE);
+	        String s[]=null;
+	        try{
+	            // -read from filePooped with Scanner class
+	            Scanner inputStream = new Scanner(file);
+	            // hashNext() loops line-by-line
+	            int i=0;
+	            while(inputStream.hasNext()){
+	                //read single line, put in string
+	                Map<String, String> m=new HashMap<String, String>();
+	                m.put(getUUID(), inputStream.next());
+	                switch (i){
+		        	case 0:
+		        		g.setVar1(m);
+		        		break;
+		        	case 1:
+		        		g.setVar2(m);
+		        		break;
+		        	case 2:
+		        		g.setVar3(m);
+		        		break;
+		        	case 3:
+		        		g.setAnswer(1);
+		        		break;
+		        	case 4:
+		        		
+		        	}
+            }
+	            // after loop, close scanner
+	            inputStream.close();
+	            
+	            
+	        for (i=0;i<s.length;i++){
+	        	
+	        }
+
+	        }catch (FileNotFoundException e){
+
+	            e.printStackTrace();
+	        }
+  return g;
+		   
+	   }
+
+	private String getUUID() {
+		String uuid = "";
+		do {
+
+			uuid = UUID.randomUUID().toString();
+
+		} while (!SetOfUUID.add(uuid));
+		return uuid;
+	}
+
+	private static CellProcessor[] getProcessors() {
+
+		final CellProcessor[] processors = new CellProcessor[] {
+				// new UniqueHashCode(), // customerNo (must be unique)
+				new NotNull(), // Movie 1
+				new NotNull(), // Movie 2
+				// new ParseDate("dd/MM/yyyy"), // birthDate
+				new NotNull(), // Movie 3
+				new ParseInt(), // Odd Movie
+				new NotNull(), // Description
+				new ParseInt() // loyaltyPoints
+		};
+
+		return processors;
+	}
+
+	private static void readWithCsvMapReader() throws Exception {
+
+		ICsvMapReader mapReader = null;
+		try {
+			mapReader = new CsvMapReader(new FileReader(QUIZ_FILE),
+					CsvPreference.STANDARD_PREFERENCE);
+
+			// the header columns are used as the keys to the Map
+			final String[] header = mapReader.getHeader(true);
+			final CellProcessor[] processors = getProcessors();
+
+			Map<String, Object> customerMap;
+			while ((customerMap = mapReader.read(header, processors)) != null) {
+				System.out.println(String.format(
+						"lineNo=%s, rowNo=%s, customerMap=%s",
+						mapReader.getLineNumber(), mapReader.getRowNumber(),
+						customerMap));
+			}
+
+		} finally {
+			if (mapReader != null) {
+				mapReader.close();
+			}
+		}
+	}
 }
